29c1bf2b3bb4247802c7e07a5b1f6866783c2f65
Jack24658735 <liujack0914@gmail.com>
2023-08-22 14:57:59 +0800

    add UVC lib and G-DINO

---
+++ scripts/score.sh
@@ -0,0 +1,15 @@
+
+
+OUTPUT_DIR=$1
+# CHECKPOINT=$2
+PY_ARGS=${@:2}  # Any arguments from the forth one are captured by this
+
+# # evaluation
+ANNO0_DIR=${OUTPUT_DIR}/"valid"/"anno_0"
+ANNO1_DIR=${OUTPUT_DIR}/"valid"/"anno_1"
+ANNO2_DIR=${OUTPUT_DIR}/"valid"/"anno_2"
+ANNO3_DIR=${OUTPUT_DIR}/"valid"/"anno_3"
+python3 eval_davis.py --results_path=${ANNO0_DIR}
+python3 eval_davis.py --results_path=${ANNO1_DIR}
+python3 eval_davis.py --results_path=${ANNO2_DIR}
+python3 eval_davis.py --results_path=${ANNO3_DIR}
