2516d8d061611fe117dd303d97b06cf9a99ac4de
Jack24658735 <liujack0914@gmail.com>
2023-11-24 02:44:55 +0800

    Fix multi-gpu for ytvos inference.(script)

--- scripts/online_ytvos_sam_mmdet.sh
+++ scripts/online_ytvos_sam_mmdet.sh
@@ -1,3 +1,10 @@
+GPUS='0,1'
+GPUS_PER_NODE=2
+export CUDA_VISIBLE_DEVICES=${GPUS}
+echo "using gpus ${GPUS}."
+now=$(date +"%T")
+echo "Current time : $now"
+echo "Current path : $PWD"
 
 OUTPUT_DIR=$1
 SAM_CHECKPOINT=$2
@@ -7,6 +14,6 @@ PY_ARGS=${@:3}  # Any arguments from the forth one are captured by this
 # inference
 # CHECKPOINT=${OUTPUT_DIR}/checkpoint.pth
 python3 inference_ytvos_online_sam_mmdet.py --with_box_refine --binary --freeze_text_encoder \
---output_dir=${OUTPUT_DIR} --online --use_SAM --sam_ckpt_path ${SAM_CHECKPOINT} --visualize ${PY_ARGS}
+--output_dir=${OUTPUT_DIR} --online --use_SAM --sam_ckpt_path ${SAM_CHECKPOINT} --visualize --ngpu=${GPUS_PER_NODE} ${PY_ARGS}
 
 echo "Working path is: ${OUTPUT_DIR}"
